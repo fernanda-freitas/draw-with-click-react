aa614cd7b29f44904bbf16a809274e9e571f28ad
fernanda freitas <idea.fernanda@gmail.com>
2023-01-03 16:17:35 +0000

    renamed variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [clickPosition, setClickPosition] = useState({});
+  const [clickPosition, setClickPosition] = useState([{}]);
 
   const handleClick = (e) => (
-    setClickPosition({positionX: e.clientX, positionY: e.clientY})
+    setClickPosition({x: e.clientX, y: e.clientY})
   )
 
   return (
     <div className="App" onClick={(handleClick)}>
-      <div className='click' style={{left:clickPosition.positionX, top:clickPosition.positionY}}></div>
+      <div className='click' style={{left: clickPosition.x, top: clickPosition.y}}></div>
     </div>
   );
 }
