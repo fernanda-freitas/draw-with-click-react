bce831509838a37765605be5c94a72ddbfaaa443
fernanda freitas <idea.fernanda@gmail.com>
2023-01-03 17:47:25 +0000

    playing with data from different usestates

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,31 @@ import './App.css';
 
 function App() {
   const [clickList, setClickList] = useState([]);
-  const [lastItem, setLastItem] = useState([])
+  const [undid, setUndid] = useState([])
 
   const handleClick = (e) => (
     setClickList([ ...clickList , {x: e.clientX, y: e.clientY }])
   )
 
-  const handleUndo = (e) => {
-    e.stopPropagation()
-    setClickList(clickList.slice(0, -1))
+  const handleUndo = (event) => {
+    event.stopPropagation()
+
+    const lastItem = clickList[clickList.length - 1]
+    setUndid((prev) => {
+      return [...prev, lastItem]
+    })
+
+    setClickList((prev) => {
+      const newArr = [...prev].slice(0,-1)
+      return newArr
+    })
+  }
+
+  console.log(clickList)
+  console.log(undid)
+
+  const handleRedo = (event) => {
+    event.stopPropagation()
   }
 
   return (
@@ -19,7 +35,8 @@ function App() {
       {clickList.map((circle) => {
         return <div className='click' style={{left: circle.x, top: circle.y}}></div>
       })}
-      <button onClick={handleUndo} type="button" className="btn btn-primary undo-action">Undo</button>
+      <button onClick={handleUndo} type="button" className="btn btn-secondary undo-action">Undo</button>
+      <button onClick={handleRedo} type="button" className="btn btn-primary redo-action">Redo</button>
     </div>
   );
 }
