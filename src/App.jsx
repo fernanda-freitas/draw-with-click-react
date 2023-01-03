c118decbe808962dd1763d0312b13e1c1968356c
fernanda freitas <idea.fernanda@gmail.com>
2023-01-03 18:06:30 +0000

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,37 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [clickList, setClickList] = useState([]);
-  const [undid, setUndid] = useState([])
+  const [clickedList, setClickedList] = useState([]);
+  const [removedList, setRemovedList] = useState([])
 
-  const handleClick = (e) => (
-    setClickList([ ...clickList , {x: e.clientX, y: e.clientY }])
-  )
+  const handleClick = (event) => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+    return setClickedList([ ...clickedList , newDot])
+  }
+
 
   const handleUndo = (event) => {
     event.stopPropagation()
-
-    setClickList((prev) => {
-      const newArr = [...prev].slice(0,-1)
-      return newArr
-    })
-
-    const lastItem = clickList[clickList.length - 1]
-    setUndid((prev) => {
-      return [...prev, lastItem]
-    })
+    setClickedList([...clickedList.slice(0,-1)])
+    const lastItem = clickedList[clickedList.length - 1]
+    setRemovedList([...removedList, lastItem])
   }
 
   const handleRedo = (event) => {
     event.stopPropagation()
 
-    setClickList((prev) => {
-      return [...prev, undid[undid.length - 1]]
+    setClickedList((prev) => {
+      return [...prev, removedList[removedList.length - 1]]
     })
   }
 
   return (
     <div className="App" onClick={handleClick}>
-      {clickList.map((circle) => {
-        return <div className='click' style={{left: circle.x, top: circle.y}}></div>
+      {clickedList.map((circle) => {
+        return <div className='click' style={{left: circle.clientX, top: circle.clientY}}></div>
       })}
       <button onClick={handleUndo} type="button" className="btn btn-secondary undo-action">Undo</button>
       <button onClick={handleRedo} type="button" className="btn btn-primary redo-action">Redo</button>
